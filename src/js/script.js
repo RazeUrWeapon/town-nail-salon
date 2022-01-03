35fd759d5dfa9c327d30b33968d6417dc702eb1c
Joseph <jwilde19@gmail.com>
2022-01-03 18:44:41 -0500

    Add scroll fade-in effect

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,9 @@ const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElements = document.querySelectorAll('.has-fade');
 const mobileMenu = document.querySelector('.header__menu');
+const scrollFadeInElements = document.querySelectorAll('.scroll-fade-in');
+
+let windowHeight = window.innerHeight;
 
 function closeHamburgerMenu() {
     body.classList.remove('noscroll');
@@ -36,3 +39,33 @@ mobileMenu.addEventListener('click', () => {
     if(header.classList.contains('open')) {closeHamburgerMenu()}
 });
 
+// Fade in on scroll effect
+function scrollFade() {
+    document.addEventListener("DOMContentLoaded", () => {
+      scrollFadeInInit(scrollFadeInElements);
+  
+      window.addEventListener("scroll", () => {
+        scrollFadeInEffect(scrollFadeInElements);
+      });
+    });
+  
+    function scrollFadeInInit(element) {
+      for (let i = 0; i < element.length; i++) {
+        let posFromTop = element[i].getBoundingClientRect().top;
+        if (posFromTop - windowHeight > 0) {
+          element[i].classList.add("is-hidden");
+        }
+      }
+    }
+  
+    function scrollFadeInEffect(element) {
+      for (let i = 0; i < element.length; i++) {
+        let posFromTop = element[i].getBoundingClientRect().top;
+        if (posFromTop - windowHeight <= 0) {
+          element[i].classList.remove("is-hidden");
+        }
+      }
+    }
+  };
+
+  scrollFade();
